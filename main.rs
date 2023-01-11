9454b71f8c3ba9e9374a38f4ef00d176e35fb838
na <81983672+yuanjv@users.noreply.github.com>
2023-01-11 15:32:49 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,15 +1,28 @@
 use std::io;
 use std::{thread, time};
-use rsautogui::keyboard::{self};
+use rsautogui::keyboard::{self,Key};
 
 fn main() {
+    let mut list = vec![];
+    let mut input=String::new();
     while true{
         print!("{}[2J", 27 as char);
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-        println!("paste here:");
-        let mut input=String::new();
-        io::stdin().read_line(&mut input).expect("ERROR>>>failed to read line");
+        println!("paste here >>>type &*( to start<<<:");
+
+        while &input != "&*(\n"{
+            list.insert(list.len(),input);
+            input="".to_string();
+            io::stdin().read_line(&mut input).expect("ERROR>>>failed to read line");
+        }
         thread::sleep(time::Duration::from_secs(3));
-        keyboard::typewrite(&input);
+        for l in &list{
+            keyboard::typewrite(&l);
+            if l!=""{
+                keyboard::key_down(Key::Return);
+            }
+        }
+        list.clear();
+        input="".to_string();
     }
 }
